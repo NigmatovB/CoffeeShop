c54a0194a0cb6b36d6ecc3730f8d10e0ef588c4c
Bekzod <nigmatovbekzod3770@gmail.com>
2022-02-01 14:19:53 +0500

    The coffee router has been fixed

--- src/routers/coffee.js
+++ src/routers/coffee.js
@@ -1,10 +1,56 @@
 const router = require('express').Router()
 const db = require('../schema/coffee')
+const upload = require('../middlewares/upload')
 
+// Getting data in DB
 router.get('/', async ( req,res ) => {
     try{
         let coffee = db.find({})
         res.send(coffee)
+    } catch( error ){
+        res.send(error)
+    }
+} )
+
+// Add data to DB
+router.post('/', upload.single('image'), async ( req,res ) => {
+    try{
+        let coffee = db.create({
+            name: req.body.name,
+            price: req.body.price,
+            time: req.body.time,
+            image: req.file.filename,
+        })
+        res.send('data added')
+    } catch( error ){
+        res.send(error)
+    }
+} )
+
+// Change the data in the DB
+router.put('/', upload.single('image'), async ( req,res ) => {
+    try{
+        let coffee = db.findOneAndUpdate({
+            _id : req.body._id
+        },{
+            name: req.body.name,
+            price: req.body.price,
+            time: req.body.time,
+            image: req.file.filename,
+        })
+        res.send('data changed')
+    } catch( error ){
+        res.send(error)
+    }
+} )
+
+// Delete data in DB
+router.put('/', async ( req,res ) => {
+    try{
+        let coffee = db.findOneAndDelete({
+            _id: req.body._
+        })
+        res.send('data deleted')
     } catch( error ){
         res.send(error)
     }
